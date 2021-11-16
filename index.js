3212f75022c62d8943c7b3feb9345d468b3c4509
ziho <ziho@kakao.com>
2021-11-17 01:08:16 +0900

    feat: url, redirect 라우트 연결

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const express = require('express')
+const express = require('express');
+const app = express();
 
-// instatiate the express app  
-const app = express()
+const connection = require('./config/db');
+connection.once('open', () => console.log('DB Connected'));
+connection.on('error', () => console.log('Error'));
 
-const PORT = process.env.PORT || 5000
-// Listen for incoming requests
-app.listen(PORT, () => console.log(`server started, listening PORT ${PORT}`))
+app.use(
+  express.json({
+    extended: false,
+  })
+);
+app.use('/', require('./routes/redirect'));
+app.use('/api/url', require('./routes/url'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`server started, listening PORT ${PORT}`));
